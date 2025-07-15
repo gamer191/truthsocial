a0b5dce28b241ef40872a7168e07b9eeeab3f347
gamer191 <83270075+gamer191@users.noreply.github.com>
2025-07-15 14:30:48 +1000

    Undo cause IPv6 still doesn’t work with GitHub Actions

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ rm -rf source_tmp
 sudo apt install torsocks
 pipx install yt-dlp[default,curl_cffi]
 # Rotate IP addresses (by restarting Tor) if we get captcha-blocked by Truth Social
-until torsocks yt-dlp --force-ipv6 --impersonate chrome https://opensource.truthsocial.com/mastodon-current.zip -o "mastodon-current.zip";do sudo service tor restart;done
+until torsocks yt-dlp --impersonate chrome https://opensource.truthsocial.com/mastodon-current.zip -o "mastodon-current.zip";do sudo service tor restart;done
 unzip mastodon-current.zip -d source_tmp
 
 mv source_tmp/open\ source source
